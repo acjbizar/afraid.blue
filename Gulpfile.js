ca34ae6b75b2cad64b3c069124ecb7141cbae7f5
ACJ <gitlab@acjs.net>
2022-01-18 21:41:43 +0100

    Build basic artwork
    Sources:
    
    - [BrandColors](https://brandcolors.net/)
    - [Statista](https://www.statista.com/statistics/272014/global-social-networks-ranked-by-number-of-users/)
    - [Wikipedia](https://en.wikipedia.org/wiki/LinkedIn)

---
+++ Gulpfile.js
@@ -0,0 +1,20 @@
+
+"use strict";
+
+const gulp = require('gulp');
+const twig = require('gulp-twig');
+const data = require('gulp-data');
+const fs = require('fs');
+
+// Compile Twig templates to HTML
+gulp.task('render', function() {
+    return gulp.src(['./src/**/*.twig', '!src/**/_*.twig'], { dot: true })
+        .pipe(data(function(file) {
+            return JSON.parse(fs.readFileSync('data.json'));
+        }))
+        .pipe(twig({
+            errorLogToConsole: true,
+            extname: false
+        }))
+        .pipe(gulp.dest('./dist/')); // output the rendered files to the "dist" directory
+});
